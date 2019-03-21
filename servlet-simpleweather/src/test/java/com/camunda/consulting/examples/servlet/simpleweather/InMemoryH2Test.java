345f8230ff5f420cae143d8ff393c6c4d234c00d
Sebastian Warnke <sw-gitlab@sebwarnke.com>
2019-03-21 10:08:30 +0100

    add servlet-simpleweather

---
+++ servlet-simpleweather/src/test/java/com/camunda/consulting/examples/servlet/simpleweather/InMemoryH2Test.java
@@ -0,0 +1,63 @@
+package com.camunda.consulting.examples.servlet.simpleweather;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.init;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.processEngine;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.complete;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.task;
+
+import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+public class InMemoryH2Test {
+
+  @ClassRule
+  @Rule
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
+
+  private static final String PROCESS_DEFINITION_KEY = "simpleweather";
+
+  static {
+    LogFactory.useSlf4jLogging(); // MyBatis
+  }
+
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
+  }
+
+  /**
+   * Just tests if the process definition is deployable.
+   */
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testParsingAndDeployment() {
+    // nothing is done here, as we just want to check for exceptions during deployment
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testHappyPath() {
+	  ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+	  
+	  assertThat(processInstance).isWaitingAt("CheckWeatherTask");
+	  processEngine().getRuntimeService().setVariable(processInstance.getId(), "is_sunny", true);
+	  
+	  complete(task());
+	  
+	  assertThat(processInstance).isEnded();
+	  
+	  // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
+  }
+
+}
